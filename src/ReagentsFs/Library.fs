13a5b32836161ba55ce5f1e1fc12e7a9d2d00059
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-03-23 12:08:40 +0200

    Next step

--- src/ReagentsFs/Library.fs
+++ src/ReagentsFs/Library.fs
@@ -1,5 +1,218 @@
 namespace ReagentsFs
 
+module Atomic =
+  open System
+  open System.Threading
+  open System.Collections.Generic
+
+  let inline inc (x: byref<int>) = Interlocked.Increment (&x)
+  let inline dec (x: byref<int>) = Interlocked.Decrement (&x)
+  let inline same (x: obj) (y: obj) = LanguagePrimitives.PhysicalEquality x y
+  let inline casi (x: byref<int>, y: _, z: _) = Interlocked.CompareExchange (&x, y, z)
+  let inline cas (x: byref<obj>, y: _, z: _) = Interlocked.CompareExchange (&x, y, z)
+  let inline casa (x: byref<obj[]>, y: _, z: _) = Interlocked.CompareExchange (&x, y, z)
+
+  let mutable ids = 0
+
+  type Atom =
+    val id: int
+    val mutable content: obj
+    new content = { id = inc (&ids) ; content = content }
+
+  type Atom<'a> (value: 'a) =
+    inherit Atom (box value)
+
+  type Descriptor =
+    {
+      mutable helpers: int
+      mutable allAtoms: Atom[]
+      mutable uniqAtoms: Atom[]
+      mutable uniqAtomsCount: int
+      mutable indices: int[]
+      mutable values: obj[]
+      mutable input: obj
+      mutable op: Op
+    }
+
+  and [<AbstractClass>] Op () =
+    abstract member Attempt: Descriptor -> unit
+
+  let atom (v: 'v) = Atom<'v> v
+
+  let [<Literal>] Acquire = 0
+  let [<Literal>] Attempt = 1
+
+  let alloc () =
+    {
+      helpers = 0
+      allAtoms = [||]
+      uniqAtoms = [||]
+      uniqAtomsCount = 0
+      indices = [||]
+      values = [||]
+      input = null
+      op = Unchecked.defaultof<_>
+    }
+
+  type [<AbstractClass>] Op<'a, 'b> () =
+    inherit Op ()
+    abstract member Atoms: Descriptor * int -> int
+    abstract member Attempt: Descriptor * obj[] * 'a * byref<'b> * int -> int
+
+  let inline attempt (this: Op<'a, 'b>) (d: Descriptor) =
+    let before = Volatile.Read (&d.values)
+    let n = d.uniqAtomsCount
+    if same d before.[n] then
+      let mutable b = Unchecked.defaultof<'b>
+      let vs = Array.zeroCreate (n + 1)
+      for i=0 to n - 1 do
+        vs.[i] <- before.[i]
+      if same before (Volatile.Read (&d.values)) then
+        this.Attempt (d, vs, unbox<'a> d.input, &b, 0) |> ignore
+        vs.[n] <- box b
+        Interlocked.CompareExchange (&d.values, vs, before) |> ignore
+
+  let inline addAtom (d: Descriptor) i r =
+    if d.allAtoms.Length <= i then
+      let allAtoms = Array.zeroCreate (i + 1)
+      for j=0 to i-1 do
+        allAtoms.[j] <- d.allAtoms.[j]
+      d.allAtoms <- allAtoms
+    d.allAtoms.[i] <- r
+    i + 1
+
+  let inline access (d: Descriptor) (vs: obj[]) i : byref<obj> =
+    &vs.[d.indices.[i]]
+
+  let load (aAtom: Atom<'a>) : Op<_, 'a> =
+    {new Op<_, 'a> () with
+      member this.Atoms (d, i) = addAtom d i aAtom
+      member this.Attempt (d, vs, _, aR, i) =
+        aR <- unbox<'a> (access d vs i)
+        i + 1
+      member this.Attempt d = attempt this d}
+
+  let store (aAtom: Atom<'a>) : Op<'a, unit> =
+    {new Op<'a, unit> () with
+      member this.Atoms (d, i) = addAtom d i aAtom
+      member this.Attempt (d, vs, a, _, i) =
+        let aR = &access d vs i
+        aR <- box<'a> a
+        i + 1
+      member this.Attempt d = attempt this d}
+
+  let update (aab: 'a * 'b -> 'a * 'c) (aAtom: Atom<'a>) : Op<'b, 'c> =
+    {new Op<'b, 'c> () with
+      member this.Atoms (d, i) = addAtom d i aAtom
+      member this.Attempt (d, vs, b, cR, i) =
+        let aR = &access d vs i
+        let (a, c) = aab (unbox<'a> aR, b)
+        aR <- box<'a> a
+        cR <- c
+        i + 1
+      member this.Attempt d = attempt this d}
+
+  let (>>) (ab: Op<'a, 'b>) (bc: Op<'b, 'c>) : Op<'a, 'c> =
+    {new Op<'a, 'c> () with
+      member this.Atoms (d, i) = bc.Atoms (d, ab.Atoms (d, i))
+      member this.Attempt (d, vs, a, c, i) =
+        let mutable b = Unchecked.defaultof<'b>
+        let i = ab.Attempt (d, vs, a, &b, i)
+        if 0 <= i then bc.Attempt (d, vs, b, &c, i) else i
+      member this.Attempt d = attempt this d}
+
+  let rec proceed (d: Descriptor) =
+    let n = d.uniqAtomsCount
+    let mutable vs = Volatile.Read (&d.values)
+    if not (same d vs.[n]) then
+      let uniqAtoms = d.uniqAtoms
+      for i = 0 to n - 1 do
+        cas (&uniqAtoms.[i].content, vs.[i], d) |> ignore
+    elif same d vs.[n-1] then
+      let mutable i = 0
+      let uniqAtoms = d.uniqAtoms
+      while i < n && same d vs.[n-1] do
+        if same d vs.[i] then
+          let atom = uniqAtoms.[i]
+          let wasBefore = Volatile.Read (&atom.content)
+          match wasBefore with
+            | :? Descriptor as other ->
+              if same d other then
+                while same d <| Volatile.Read (&vs.[i]) do
+                  Thread.Yield () |> ignore
+                i <- i + 1
+              else
+                help other (&atom.content)
+            | _ ->
+              let vs' = Volatile.Read (&d.values)
+              if same vs' vs then
+                let wasAfter = cas (&atom.content, d, wasBefore)
+                if same wasAfter wasBefore then
+                  Volatile.Write (&vs.[i], wasAfter)
+                  i <- i + 1
+                elif same wasAfter d then
+                  while same d <| Volatile.Read (&vs.[i]) do
+                    Thread.Yield () |> ignore
+                  i <- i + 1
+              else
+                vs <- vs'
+        else
+          i <- i + 1
+      proceed d
+    else
+      d.op.Attempt d
+      proceed d
+
+  and help (d: Descriptor) (from: byref<obj>) =
+    inc (&d.helpers) |> ignore
+    if same (Volatile.Read (&from)) d then
+      proceed d
+    dec (&d.helpers) |> ignore
+
+  let inline prepare (d: Descriptor) (ab: Op<'a, 'b>) (a: 'a) =
+    d.input <- box a
+    d.op <- ab :> Op
+
+    let allAtomsCount = ab.Atoms (d, 0)
+
+    if d.uniqAtoms.Length < allAtomsCount then
+      d.uniqAtoms <- Array.zeroCreate allAtomsCount
+      d.indices <- Array.zeroCreate allAtomsCount
+      d.values <- Array.zeroCreate (allAtomsCount + 1)
+
+    d.uniqAtomsCount <- 0
+    for i = 0 to allAtomsCount - 1 do
+      let ref = d.allAtoms.[i]
+      let mutable j = 0
+      while j < d.uniqAtomsCount && d.uniqAtoms.[j].id < ref.id do
+        j <- j + 1
+      if j = d.uniqAtomsCount || not (same d.uniqAtoms.[j] ref) then
+        for k = d.uniqAtomsCount downto j + 1 do
+          d.uniqAtoms.[k] <- d.uniqAtoms.[k-1]
+        d.uniqAtoms.[j] <- ref
+        d.uniqAtomsCount <- d.uniqAtomsCount + 1
+
+    for i = 0 to allAtomsCount - 1 do
+      let ref = d.allAtoms.[i]
+      let mutable j = 0
+      while not (same d.uniqAtoms.[j] ref) do
+        j <- j + 1
+      d.indices.[i] <- j
+
+    for i = 0 to d.uniqAtomsCount do
+      d.values.[i] <- d :> obj
+
+  let inline waitFree (d: Descriptor) =
+    while 0 <> Volatile.Read (&d.helpers) do
+      Thread.Yield () |> ignore
+
+  let atomically (d: Descriptor) (ab: Op<'a, 'b>) (a: 'a) : 'b =
+    prepare d ab a
+    proceed d
+    let result = unbox<'b> d.values.[d.uniqAtomsCount]
+    waitFree d
+    result
+
 module CASN =
   open System
   open System.Threading
--- src/ReagentsFsTest/Program.fs
+++ src/ReagentsFsTest/Program.fs
@@ -4,71 +4,141 @@ open System.Diagnostics
 
 open ReagentsFs
 
-let verify b =
-  if not b then failwith "Failed"
+let verifyEq actual expected =
+  if actual <> expected then failwithf "%A <> %A" actual expected
 
-let stressTest () =
-  let atoms = Array.init 8 (fun _ -> CASN.atom 0)
-  let n = Environment.ProcessorCount
-  printfn "ProcessorCount = %d" n
-  if not Runtime.GCSettings.IsServerGC then
-    printfn "Not using server GC!"
-  let nLive = ref n
-  use startBarrier = new Barrier (!nLive)
-  let nAttempts = ref 0L
-  let nSuccesses = ref 0L
-  let totalTime = ref (TimeSpan 0L)
-  for i=1 to !nLive do
-    let t = Thread (fun () ->
-      let mutable nS = 0L
-      let mutable nA = 0L
-      let rnd = System.Random ()
-      let casn = CASN.alloc 3
-      startBarrier.SignalAndWait ()
-      let watch = Stopwatch ()
-      watch.Start ()
-      while nS < 4000000L do
-        let i = rnd.Next atoms.Length
-        let mutable j = i
-        while j = i do
-          j <- rnd.Next atoms.Length
-        let mutable k = i
-        while k = i || k = j do
-          k <- rnd.Next atoms.Length
-        let iVal = CASN.load atoms.[i]
-        let jVal = CASN.load atoms.[j]
-        let kVal = CASN.load atoms.[k]
-        CASN.op casn 0 atoms.[i] (jVal - 2) iVal
-        CASN.op casn 1 atoms.[j] (kVal + 1) jVal
-        CASN.op casn 2 atoms.[k] (iVal + 1) kVal
-        if CASN.casn casn then
+let nAtoms = 8
+let nOps = 4000000L
+
+module TestCASN =
+  open CASN
+  let stress () =
+    let atoms = Array.init nAtoms (fun _ -> atom 0)
+    let n = Environment.ProcessorCount
+    printfn "ProcessorCount = %d" n
+    if not Runtime.GCSettings.IsServerGC then
+      printfn "Not using server GC!"
+    let nLive = ref n
+    use startBarrier = new Barrier (!nLive)
+    let nAttempts = ref 0L
+    let nSuccesses = ref 0L
+    let totalTime = ref (TimeSpan 0L)
+    for i=1 to !nLive do
+      let t = Thread (fun () ->
+        let mutable nS = 0L
+        let mutable nA = 0L
+        let rnd = System.Random ()
+        let desc = alloc 2
+        startBarrier.SignalAndWait ()
+        let watch = Stopwatch ()
+        watch.Start ()
+        while nS < nOps do
+          let i = rnd.Next atoms.Length
+          let mutable j = i
+          while j = i do
+            j <- rnd.Next atoms.Length
+          //let mutable k = i
+          //while k = i || k = j do
+          //  k <- rnd.Next atoms.Length
+          let iVal = load atoms.[i]
+          let jVal = load atoms.[j]
+          //let kVal = load atoms.[k]
+          op desc 0 atoms.[i] (jVal - 1) iVal
+          op desc 1 atoms.[j] (iVal + 1) jVal
+          //op desc 2 atoms.[k] (iVal + 1) kVal
+          if casn desc then
+            nS <- nS + 1L
+          nA <- nA + 1L
+          if 0L = (nA &&& 0xFFFFFL) then
+            printf "."
+        printf "(%d)" i
+        Interlocked.Add (nAttempts, nA) |> ignore
+        Interlocked.Add (nSuccesses, nS) |> ignore
+        let took = watch.Elapsed
+        lock nLive <| fun () ->
+          totalTime := !totalTime + took
+          nLive := !nLive - 1
+          Monitor.PulseAll nLive)
+      t.Start()
+    lock nLive <| fun () ->
+      while !nLive <> 0 do
+        Monitor.Wait nLive |> ignore
+    let sum = atoms |> Array.sumBy load
+    printfn "tot = %d, suc = %d, t = %A, suc / sec / thread = %f"
+      (!nAttempts)
+      (!nSuccesses)
+      (!totalTime)
+      (float (!nSuccesses) / (!totalTime).TotalSeconds)
+    verifyEq sum 0
+
+module TestAtomic =
+  open Atomic
+
+  let smoke () =
+    let xA = atom 1
+    let yA = atom 2
+    let d = alloc ()
+    atomically d (load yA >> update (fun (x, y) -> (y+1, x-1)) xA >> store yA) ()
+    verifyEq (atomically d (load xA) ()) 3
+    verifyEq (atomically d (load yA) ()) 0
+
+  let stress () =
+    let atoms = Array.init nAtoms (fun _ -> atom 0)
+    let n = Environment.ProcessorCount
+    printfn "ProcessorCount = %d" n
+    if not Runtime.GCSettings.IsServerGC then
+      printfn "Not using server GC!"
+    let nLive = ref n
+    use startBarrier = new Barrier (!nLive)
+    let nSuccesses = ref 0L
+    let totalTime = ref (TimeSpan 0L)
+    for i=1 to !nLive do
+      let t = Thread (fun () ->
+        let mutable nS = 0L
+        let mutable nA = 0L
+        let rnd = System.Random ()
+        let desc = alloc ()
+        startBarrier.SignalAndWait ()
+        let watch = Stopwatch ()
+        watch.Start ()
+        while nS < nOps do
+          let i = rnd.Next atoms.Length
+          let mutable j = i
+          while j = i do
+            j <- rnd.Next atoms.Length
+          atomically
+            desc
+            (load atoms.[i] >>
+             update (fun (x, y) -> (y-1, x+1)) atoms.[j] >>
+             store atoms.[i])
+            ()
           nS <- nS + 1L
-        nA <- nA + 1L
-        if 0L = (nA &&& 0xFFFFFL) then
-          printf "."
-      printf "(%d)" i
-      Interlocked.Add (nAttempts, nA) |> ignore
-      Interlocked.Add (nSuccesses, nS) |> ignore
-      let took = watch.Elapsed
-      lock nLive <| fun () ->
-        totalTime := !totalTime + took
-        nLive := !nLive - 1
-        Monitor.PulseAll nLive)
-    t.Start()
-  lock nLive <| fun () ->
-    while !nLive <> 0 do
-      Monitor.Wait nLive |> ignore
-  let sum = atoms |> Array.sumBy CASN.load
-  printfn "tot = %d, suc = %d, t = %A, suc / sec / thread = %f"
-    (!nAttempts)
-    (!nSuccesses)
-    (!totalTime)
-    (float (!nSuccesses) / (!totalTime).TotalSeconds)
-  verify (sum = 0)
+          if 0L = (nS &&& 0xFFFFFL) then
+            printf "."
+        printf "(%d)" i
+        Interlocked.Add (nSuccesses, nS) |> ignore
+        let took = watch.Elapsed
+        lock nLive <| fun () ->
+          totalTime := !totalTime + took
+          nLive := !nLive - 1
+          Monitor.PulseAll nLive)
+      t.Start()
+    lock nLive <| fun () ->
+      while !nLive <> 0 do
+        Monitor.Wait nLive |> ignore
+    let sum = atoms |> Array.sumBy (fun a -> atomically (alloc ()) (load a) ())
+    printfn "suc = %d, t = %A, suc / sec / thread = %f"
+      (!nSuccesses)
+      (!totalTime)
+      (float (!nSuccesses) / (!totalTime).TotalSeconds)
+    verifyEq sum 0
 
 [<EntryPoint>]
 let main argv =
-  stressTest ()
+  TestAtomic.smoke ()
+  TestAtomic.stress ()
+
+  //TestCASN.stress ()
 
   printfn "Big success!"
   0
